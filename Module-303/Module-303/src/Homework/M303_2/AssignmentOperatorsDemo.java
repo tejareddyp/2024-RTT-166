e35ed631bd86af5111df8db5e64a4928d9a3c7ee
teja <tejaswini.reddy@gmail.com>
2024-10-04 05:33:00 -0500

    Homework

--- Module-303/Module-303/src/Homework/M303_2/AssignmentOperatorsDemo.java
+++ Module-303/Module-303/src/Homework/M303_2/AssignmentOperatorsDemo.java
@@ -19,6 +19,7 @@ public class AssignmentOperatorsDemo {
 
         int x, y = 10, z = 5;
         x = y + z;
+
         System.out.println("+ operator resulted in " + x);
         x = y - z;
         System.out.println("- operator resulted in " + x);
@@ -41,13 +42,17 @@ public class AssignmentOperatorsDemo {
         int tooSmall = Integer.MIN_VALUE - 1; // 2147483647 which is
         // Integer.MAX_VALUE.
 
+
         System.out.println("tooBig becomes " + tooBig);
         System.out.println("tooSmall becomes " + tooSmall);
+
         System.out.println(4.0 / 0.0); // Prints: Infinity
         System.out.println(-4.0 / 0.0); // Prints: -Infinity
         System.out.println(0.0 / 0.0); // Prints: NaN
+
         double d1 = 12 / 8; // result: 1 by integer division. The value of d1 becomes 1.0.
-        double d2 = 12.0F / 8; // result: 1.5
+        double d2 = 12.0F / 8; // result: 1.
+
         System.out.println("d1 is " + d1);
         System.out.println("d2 is " + d2);
 
