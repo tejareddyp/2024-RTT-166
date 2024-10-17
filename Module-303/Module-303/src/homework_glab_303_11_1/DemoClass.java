2b1b97396df72dc6b6a68ea1c255f54052fb8f4a
teja <tejaswini.reddy@gmail.com>
2024-10-16 19:46:25 -0500

    Homework

---
+++ Module-303/Module-303/src/homework_glab_303_11_1/DemoClass.java
@@ -0,0 +1,11 @@
+package homework_glab_303_11_1;
+
+public class DemoClass {
+
+    // create a generics method
+    public <T> void genericsMethod(T data) {
+        System.out.println("Generics Method:");
+        System.out.println("Data Passed: " + data);
+    }
+
+}
---
+++ Module-303/Module-303/src/homework_glab_303_11_1/MyRunner.java
@@ -0,0 +1,4 @@
+package homework_glab_303_11_1;
+
+public class myRunner {
+}
