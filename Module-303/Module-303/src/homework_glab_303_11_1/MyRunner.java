2f90c6612651bfb4d1ff034914173c61684781d8
teja <tejaswini.reddy@gmail.com>
2024-10-16 19:48:30 -0500

    Homework

--- Module-303/Module-303/src/homework_glab_303_11_1/MyRunner.java
+++ Module-303/Module-303/src/homework_glab_303_11_1/MyRunner.java
@@ -1,4 +1,14 @@
 package homework_glab_303_11_1;
 
-public class myRunner {
+public class MyRunner {
+
+    public static void main(String[] args) {
+        DemoClass dObj = new DemoClass();
+        dObj.genericsMethod(25); // passing int
+        dObj.genericsMethod("Per Scholas"); // passing String
+        dObj.genericsMethod(2563.5); // passing float
+        dObj.genericsMethod('H');
+
+    }
+
 }
